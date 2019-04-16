58146939ec04edcb34a684455083a36570ec915c
Maoma <maoma@gmail.com>
2019-04-16 01:24:58 -0500

    upd

--- public/danilo/assets/js/functions.js
+++
@@ -1,9 +0,0 @@
-String.prototype.sprintf = function(){
-	var args = arguments, counter = 0;
-	//return this.replace(/%(\d+)\$s/gi,'{$1}').replace(/\{(\d+)\}/gi, function(a,number){
-	return this.replace(/%(?:(\d+)\$)?s/gi,function(a,b){
-		return typeof b !== "undefined" ? "{"+b+"}" : "{"+ (++counter) +"}"
-	}).replace(/\{(\d+)\}/gi, function(a,number){
-		return args[+number - 1] || a;
-	});
-};
